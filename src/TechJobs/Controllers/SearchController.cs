c4ec9efbac41794657899a1769368b8e49778fdd
Pat McEvoy <pat.mcevoy@hotmail.com>
2018-05-31 23:08:46 -0500

    Finish TechJobMVC

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -16,5 +16,23 @@ namespace TechJobs.Controllers
         // TODO #1 - Create a Results action method to process 
         // search request and display results
 
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchType.Equals("all"))
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByValue(searchTerm);
+                ViewBag.jobs = jobs;
+            }
+
+            else
+            {
+                List<Dictionary<string, string>> jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.jobs = jobs;
+            }
+
+            ViewBag.columns = ListController.columnChoices;
+
+            return View("Index");
+        }
     }
 }
